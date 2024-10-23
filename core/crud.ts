039e8ba01b498ac8658a72b5af6297c8f3d7e86e
Daniel Catania Rodrigues <78505788+DanielCatania@users.noreply.github.com>
2024-10-23 19:51:04 +0000

    aula 2.2

--- core/crud.ts
+++ core/crud.ts
@@ -1,53 +1,104 @@
 console.log("CRUD START");
 import fs from "fs";
+import crypto from "crypto";
 
 const DB_FILE_PATH = "./core/db";
 
-interface noteData {
+const notFoundError = { status: 404, message: "Not found" };
+
+function getDB() {
+  try {
+    const db = JSON.parse(fs.readFileSync(DB_FILE_PATH).toString());
+    if (!db.toDos || !Array.isArray(db.toDos))
+      throw new Error("DB not compatible");
+    return db;
+  } catch (error) {
+    NEW_DB();
+  }
+}
+
+function updateDB(data: Object) {
+  if (typeof data !== "object") throw new Error("the DB must be a json object");
+  fs.writeFileSync(DB_FILE_PATH, JSON.stringify(data, null, 2));
+}
+
+function NEW_DB() {
+  const newDB = { toDos: [] };
+
+  updateDB(newDB);
+  return newDB;
+}
+
+interface toDo {
   id: string;
   content: string;
-  createdAt: Date;
+  createdAt: string;
+  isDone: boolean;
 }
 
-function create(content: string) {
-  const noteData: noteData = {
+function createToDo(content: string) {
+  const toDo: toDo = {
     id: crypto.randomUUID(),
     content,
-    createdAt: new Date(),
+    createdAt: new Date().toISOString(),
+    isDone: false,
   };
-  if (!fs.existsSync(DB_FILE_PATH)) {
-    const db = { notes: [noteData] };
 
-    fs.writeFileSync(DB_FILE_PATH, JSON.stringify(db));
-  } else {
-    const dbFile = fs.readFileSync(DB_FILE_PATH).toString();
-    const data = JSON.parse(dbFile);
+  const db = getDB();
 
-    data.notes.push(noteData);
+  db.toDos.push(toDo);
+  updateDB(db);
 
-    fs.writeFileSync(DB_FILE_PATH, JSON.stringify(data));
-  }
-
-  return noteData;
+  return toDo;
 }
 
-function readAll() {
-  if (!fs.existsSync(DB_FILE_PATH))
-    return { staus: 200, message: "nothing to read" };
+function readAllToDos() {
+  const db = getDB();
 
-  const file = fs.readFileSync(DB_FILE_PATH).toString();
-  const data = JSON.parse(file);
-
-  return data;
+  return db.toDos;
 }
 
-function findById(id: string) {
-  if (!fs.existsSync(DB_FILE_PATH)) return { staus: 404, message: "Not found" };
+function findToDoById(id: string) {
+  const db = getDB();
 
-  const dbFile = fs.readFileSync(DB_FILE_PATH).toString();
-  const data = JSON.parse(dbFile);
+  const toDo = db.toDos.find((toDo: toDo) => toDo.id === id);
 
-  const note = data.notes.filter((note: noteData) => note.id === id);
+  if (!toDo) return notFoundError;
 
-  return note[0];
+  return toDo;
 }
+
+function changeIsDoneById(id: string) {
+  const toDo = findToDoById(id);
+
+  if (toDo === notFoundError) return notFoundError;
+
+  const db = getDB();
+  const index = db.toDos.findIndex(
+    (toDoInTest: toDo) => toDoInTest.id === toDo.id
+  );
+
+  toDo.isDone = !toDo.isDone;
+  db.toDos[index] = toDo;
+
+  updateDB(db);
+
+  return toDo;
+}
+
+// NEW_DB();
+// console.log(readAllToDos());
+
+// const { id } = createToDo("One");
+// createToDo("Two");
+// console.log(readAllToDos());
+
+// console.log(findToDoById("One")); //  error
+// console.log(changeIsDoneById("One")); //  error
+
+// console.log(findToDoById(id));
+// changeIsDoneById(id);
+// console.log(readAllToDos());
+
+// changeIsDoneById(id);
+// console.log(findToDoById(id));
