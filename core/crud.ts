ae612f023e37be8b41fa9d87a2d0a2d51992e136
Daniel Catania Rodrigues <78505788+DanielCatania@users.noreply.github.com>
2024-11-12 00:27:25 +0000

    a2.3

--- core/crud.ts
+++ core/crud.ts
@@ -4,43 +4,53 @@ import crypto from "crypto";
 
 const DB_FILE_PATH = "./core/db";
 
-const notFoundError = { status: 404, message: "Not found" };
+const notFoundError = '{ status: 404, message: "Not found To Do" }';
 
-function getDB() {
+interface IToDo {
+  id: string;
+  content: string;
+  createdAt: string;
+  isDone: boolean;
+  updatedAt: string;
+}
+
+interface IDB {
+  toDos: IToDo[];
+}
+
+function getDB(): IDB {
   try {
     const db = JSON.parse(fs.readFileSync(DB_FILE_PATH).toString());
     if (!db.toDos || !Array.isArray(db.toDos))
       throw new Error("DB not compatible");
     return db;
   } catch (error) {
-    NEW_DB();
+    return NEW_DB();
   }
 }
 
-function updateDB(data: Object) {
+function updateDB(data: Object): void {
   if (typeof data !== "object") throw new Error("the DB must be a json object");
   fs.writeFileSync(DB_FILE_PATH, JSON.stringify(data, null, 2));
 }
 
-function NEW_DB() {
+function NEW_DB(): IDB {
   const newDB = { toDos: [] };
 
   updateDB(newDB);
   return newDB;
 }
 
-interface toDo {
-  id: string;
-  content: string;
-  createdAt: string;
-  isDone: boolean;
-}
+function createToDo(content: string): IToDo {
+  if (!content || typeof content !== "string" || content.trim() === "") {
+    throw new Error("Invalid content");
+  }
 
-function createToDo(content: string) {
-  const toDo: toDo = {
+  const toDo: IToDo = {
     id: crypto.randomUUID(),
     content,
     createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
     isDone: false,
   };
 
@@ -52,53 +62,77 @@ function createToDo(content: string) {
   return toDo;
 }
 
-function readAllToDos() {
+function readAllToDos(): IToDo[] {
   const db = getDB();
 
   return db.toDos;
 }
 
-function findToDoById(id: string) {
+function findToDoById(id: string): IToDo | null {
   const db = getDB();
 
-  const toDo = db.toDos.find((toDo: toDo) => toDo.id === id);
+  const toDo = db.toDos.find((toDo: IToDo) => toDo.id === id);
 
-  if (!toDo) return notFoundError;
-
-  return toDo;
+  return toDo || null;
 }
 
-function changeIsDoneById(id: string) {
-  const toDo = findToDoById(id);
-
-  if (toDo === notFoundError) return notFoundError;
+function updateToDo(id: string, toDoUpdates: Partial<IToDo>): IToDo {
+  if (toDoUpdates.id || toDoUpdates.createdAt)
+    throw new Error("you can't update the id or createdAt field");
 
   const db = getDB();
-  const index = db.toDos.findIndex(
-    (toDoInTest: toDo) => toDoInTest.id === toDo.id
-  );
+  const toDoIndex = db.toDos.findIndex((toDo: IToDo) => toDo.id === id);
 
-  toDo.isDone = !toDo.isDone;
-  db.toDos[index] = toDo;
+  if (toDoIndex === -1) throw new Error(notFoundError);
 
+  const currentToDo = db.toDos[toDoIndex];
+  const updatedToDo: IToDo = {
+    ...currentToDo,
+    ...toDoUpdates,
+    updatedAt: new Date().toISOString(),
+  };
+
+  db.toDos[toDoIndex] = updatedToDo;
   updateDB(db);
 
-  return toDo;
+  return updatedToDo;
 }
 
-// NEW_DB();
-// console.log(readAllToDos());
+function changeIsDoneById(id: string): IToDo {
+  const db = getDB();
+  const toDoIndex = db.toDos.findIndex((toDo: IToDo) => toDo.id === id);
 
-// const { id } = createToDo("One");
-// createToDo("Two");
-// console.log(readAllToDos());
+  if (toDoIndex === -1) throw new Error(notFoundError);
 
-// console.log(findToDoById("One")); //  error
-// console.log(changeIsDoneById("One")); //  error
+  const currentToDo = db.toDos[toDoIndex];
+  const updatedToDo: IToDo = {
+    ...currentToDo,
+    isDone: !currentToDo.isDone,
+  };
 
-// console.log(findToDoById(id));
-// changeIsDoneById(id);
-// console.log(readAllToDos());
+  db.toDos[toDoIndex] = updatedToDo;
+  updateDB(db);
 
-// changeIsDoneById(id);
-// console.log(findToDoById(id));
+  return updatedToDo;
+}
+
+function updateContentById(id: string, content: string): IToDo {
+  return updateToDo(id, { content });
+}
+
+NEW_DB();
+
+const toDo = createToDo("To Do");
+console.log("FIRST READ", readAllToDos());
+
+updateToDo(toDo.id, {
+  content: "TEST",
+  isDone: true,
+});
+console.log("SECOND READ", readAllToDos());
+
+changeIsDoneById(toDo.id);
+console.log("THIRD READ", readAllToDos());
+
+updateContentById(toDo.id, "TEST 2");
+console.log("FOURTH READ", readAllToDos());
